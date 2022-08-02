7722117a9ce58f0ad7b04597459f0fe437033cc4
Christelle Konsa <christelle.konsa@hotmail.com>
2022-08-02 18:10:50 +0300

    deploying again-again

--- snakegame.js
+++ snakegame.js
@@ -139,21 +139,4 @@ window.addEventListener("keydown", (e) => {
       inputDir.y = 0;
       break;
   }
-
-  document.onkeydown = function (e) {
-    switch (e.keyCode) {
-      case 37:
-        alert("left");
-        break;
-      case 38:
-        alert("up");
-        break;
-      case 39:
-        alert("right");
-        break;
-      case 40:
-        alert("down");
-        break;
-    }
-  };
 });
